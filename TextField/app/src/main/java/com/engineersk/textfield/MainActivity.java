d57069b2e390918c0373799091d2ba56d8225c06
EngineerSk <eskayoriade@gmail.com>
2020-10-19 12:58:03 +0100

    TextField project completed

---
+++ TextField/app/src/main/java/com/engineersk/textfield/MainActivity.java
@@ -0,0 +1,72 @@
+package com.engineersk.textfield;
+
+import androidx.annotation.LongDef;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final String TAG = getClass().getSimpleName();
+
+    private EditText mUsernameField;
+    private EditText mPasswordField;
+    private TextInputLayout mUsernameTextInputLayout;
+    private TextInputLayout mPasswordTextInputLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mUsernameField = findViewById(R.id.username_textField);
+        mPasswordField = findViewById(R.id.password_textField);
+
+        mUsernameTextInputLayout = findViewById(R.id.username_textInputLayout);
+        mUsernameField.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(mUsernameField.getText().toString().isEmpty()){
+                    mUsernameTextInputLayout.setErrorEnabled(true);
+                    mUsernameTextInputLayout.setError("please enter your username");
+                }else{
+                    mUsernameTextInputLayout.setErrorEnabled(false);
+                }
+            }
+        });
+
+        mUsernameField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(mUsernameField.getText().toString().isEmpty()){
+                    mUsernameTextInputLayout.setErrorEnabled(true);
+                    mUsernameTextInputLayout.setError("Please enter your username!!!");
+                }else{
+                    mUsernameTextInputLayout.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mPasswordTextInputLayout = findViewById(R.id.password_textInputLayout);
+        mPasswordTextInputLayout.setCounterEnabled(true);
+        mPasswordTextInputLayout.setCounterMaxLength(8);
+    }
+}
